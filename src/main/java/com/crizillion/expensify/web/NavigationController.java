fcd84d793b6b2cfb8280810315f1380496007fea
Praveen <Praveen@PRAVEENJ>
2017-01-10 00:38:54 +0530

    Spring security configured.

--- src/main/java/com/crizillion/expensify/web/NavigationController.java
+++ src/main/java/com/crizillion/expensify/web/NavigationController.java
@@ -1,14 +1,19 @@
 package com.crizillion.expensify.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Controller("/")
-public class NavigationController {
+@Controller
+@Configuration
+public class NavigationController extends WebMvcConfigurerAdapter{
 	
-	@RequestMapping("/dashboard")
-	public String dashboard(){
-		return "dashboard";
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("redirect:/dashboard");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/dashboard").setViewName("dashboard");
 	}
-
+	
 }
